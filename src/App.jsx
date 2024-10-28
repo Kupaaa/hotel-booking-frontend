d9c83cfe4c93c5b58f6bc0f3c7c502a07f4d5776
Asanga Adikari <146011665+Kupaaa@users.noreply.github.com>
2024-10-29 03:22:04 +0530

    Create CategoriesPage component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AdminPage from "./pages/admin-page/adminPage";
 import HomePage from "./pages/client-page/homePage";
 import Test from "./components/test/test";
 import LoginPage from "./pages/login/login";
+import CategoriesPage from "./pages/client-page/categories";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
 <Route path="/admin/*" element={<AdminPage/>}/>
 <Route path="/test/*" element={<Test/>}/>
 <Route path="/login/*" element={<LoginPage/>}/>
+<Route path="/categories/*" element={<CategoriesPage/>}/>
 </Routes>
 </BrowserRouter>
   );
